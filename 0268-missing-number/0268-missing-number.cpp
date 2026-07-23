c4816fad84eaccb92a46f079b8fd5bdd826af376
Arnav Kakkar <142913149+arnavkakkar@users.noreply.github.com>
2026-07-23 16:59:06 +0530

    Time: 19 ms (7.41%), Space: 28.1 MB (5.01%) - LeetHub

--- 0268-missing-number/0268-missing-number.cpp
+++ 0268-missing-number/0268-missing-number.cpp
@@ -7,7 +7,7 @@ public:
         }
 
         for(int i = 1; i <= nums.size(); i++){
-            if(freq[i] == 0){
+            if(freq.find(i) == freq.end()){
                 return i;
             }
         }
